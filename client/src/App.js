04e505e1145582e2028ff9c9a2c8de8917461176
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-29 17:30:07 -0400

    Installed and set up Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,38 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//integrating Apollo into the front end of the application
+//ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
+//ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
+//InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+//createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// Establishing the connection to the back-end server's /graphql endpoint.
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+//use the ApolloClient() constructor to instantiate the Apollo Client instance and create the connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
